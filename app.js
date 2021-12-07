3da36036a8ec2246a89ddf016dea4ca7ce26bce6
Grant SoSouth <grantsosouth@Grants-Air.attlocal.net>
2021-12-07 16:04:01 -0600

    added models and routes

--- app.js
+++ app.js
@@ -5,28 +5,57 @@ const cors = require('cors');
 const path = require('path')
 const port = process.env.PORT || 50080
 
+
+//KEKE
+
 router.get('/kekeblue', function (req, res) {
-    res.sendFile(__dirname + '/public/kekeblue.html')
+    res.sendFile(__dirname + '/public/kekehtml/kekeblue.html')
 })
 
 router.get('/kekegreen', function (req, res) {
-    res.sendFile(__dirname + '/public/kekegreen.html')
+    res.sendFile(__dirname + '/public/kekehtml/kekegreen.html')
 })
 
 router.get('/kekehoney', function (req, res) {
-    res.sendFile(__dirname + '/public/kekehoney.html')
+    res.sendFile(__dirname + '/public/kekehtml/kekehoney.html')
 })
 
 router.get('/kekeredmarble', function (req, res) {
-    res.sendFile(__dirname + '/public/kekeredmarble.html')
+    res.sendFile(__dirname + '/public/kekehtml/kekeredmarble.html')
 })
 
 router.get('/kekexplode', function (req, res) {
-    res.sendFile(__dirname + '/public/kekexplode.html')
+    res.sendFile(__dirname + '/public/kekehtml/kekexplode.html')
 })
 
 router.get('/slimthugcoin', function (req, res) {
-    res.sendFile(__dirname + '/public/slimthugcoin.html')
+    res.sendFile(__dirname + '/public/kekehtml/kekegoldcoin.html')
+})
+
+//Slim
+
+router.get('/slimdarkblue', function (req, res) {
+    res.sendFile(__dirname + '/public/slimhtml/slimdarkblue.html')
+})
+
+router.get('/slimbluemarble', function (req, res) {
+    res.sendFile(__dirname + '/public/slimhtml/slimbluemarble.html')
+})
+
+router.get('/slimdarkred', function (req, res) {
+    res.sendFile(__dirname + '/public/slimhtml/slimdarkred.html')
+})
+
+router.get('/slimflower', function (req, res) {
+    res.sendFile(__dirname + '/public/slimhtml/slimflower.html')
+})
+
+router.get('/slimsplitcreamy', function (req, res) {
+    res.sendFile(__dirname + '/public/slimhtml/slimsplitcreamy.html')
+})
+
+router.get('/slimgoldcoin', function (req, res) {
+    res.sendFile(__dirname + '/public/slimhtml/slimgoldcoin.html')
 })
 
 app.use(router);
