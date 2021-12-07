c63d8d141f1d009c1e4c7d95fec4fe0fef40dfe0
Grant SoSouth <grantsosouth@Grants-Air.attlocal.net>
2021-12-07 10:11:57 -0600

    change port

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
 app.listen(port, () =>
-    console.log('Up and running')
+    console.log('Up and running on port' + port)
 );
