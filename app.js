18df696759b87398d429c32a9882c1203dcffe93
Grant SoSouth <grantsosouth@Grants-Air.attlocal.net>
2021-12-08 08:57:52 -0600

    test branch

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ metadataRouter.get('/slimdarkblue', function (req, res) {
         "image": "https://nfttesttest.herokuapp.com/assets/Lil keke GLBs/ Keke Blue.jpeg",
         "description": "PLACEHOLDER",
         "name": "TestItem",
-        "animation_url": "http://sosouth.net:50080/vinyls/slimdarkblue"
+        "animation_url": "https://nfttesttest.herokuapp.com/vinyls/slimdarkblue"
     }
     res.setHeader('content-type', 'application/json');
     res.send(JSON.stringify(data));
