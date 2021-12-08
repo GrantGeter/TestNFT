6dcf9fa23a71545a939b3b2fa11447c09b054c6e
Grant SoSouth <grantsosouth@Grants-Air.attlocal.net>
2021-12-08 10:49:36 -0600

    https

--- app.js
+++ app.js
@@ -83,10 +83,10 @@ coinRouter.get('/slimgoldcoin', function (req, res) {
 
 metadataRouter.get('/kekeblue', function (req, res) {
     const data = {
-        "image": "",
-        "description": "",
-        "name": "",
-        "animation_url": ""
+        "image": "https://sosouth.net:50080/assets/LilkekeGLBs/KekeBlue.jpeg",
+        "description": "Placeholder",
+        "name": "Test Item",
+        "animation_url": "https://sosouth.net:50080/vinyls/kekeblue"
     }
 })
 
